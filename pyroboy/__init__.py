37109fd807c7fdf8ab38a3a2a9542b1ba4995aaf
missxa <aliona.kharchenko@gmail.com>
2019-01-21 03:06:31 +0100

    added emotions publisher

--- pyroboy/__init__.py
+++ pyroboy/__init__.py
@@ -1,18 +1,25 @@
 import rclpy
-from  pyroboy.speech_synthesis import TTS
+from pyroboy.speech_synthesis import TTS
 from pyroboy.speech_recognition import STT
+from pyroboy.common_definitions import emotion_pub
 
 class Pyroboy():
     def __init__(self, node):
         self.node = node
         self.tts = None
         self.stt = None
+        self.emotion_publisher = None
 
     def init_tts(self):
         self.tts = TTS(self.node)
 
     def init_stt(self):
         self.stt = STT(self.node)
+
+    def init_face(self):
+        self.emotion_publisher = node.create_publisher(emotion_pub.type,
+                                                    emotion_pub.name)
+
 
 def say(text, language="en"):
     if pr.tts is None:
@@ -23,6 +30,12 @@ def listen():
     if pr.stt is None:
         pr.init_stt()
     return pr.stt.recognize_speech()
+
+def show_emotion(emotion):
+    if pr.emotion_publisher is None:
+        pr.init_face()
+    msg = emotion_pub.type(emotion=emotion)
+    return pr.emotion_publisher.publish(msg)
 
 rclpy.init()
 node = rclpy.create_node('pyroboy')
--- pyroboy/common_definitions.py
+++ pyroboy/common_definitions.py
@@ -1,11 +1,15 @@
 import rclpy
 from roboy_cognition_msgs.srv import Talk, RecognizeSpeech
+from roboy_control_msgs.msg import Emotion
 
-class ServiceClientDefinition:
+class RosHandlerDefinition:
     def __init__(self, type, name):
         self.type = type
         self.name = name
 
-speech_synthesis_en_srv = ServiceClientDefinition(Talk, '/roboy/cognition/speech/synthesis/talk')
-speech_synthesis_de_srv = ServiceClientDefinition(Talk, '/roboy/cognition/speech/synthesis/talk/german')
-speech_recognition_srv = ServiceClientDefinition(RecognizeSpeech, '/roboy/cognition/speech/recognition')
+
+speech_synthesis_en_srv = RosHandlerDefinition(Talk, '/roboy/cognition/speech/synthesis/talk')
+speech_synthesis_de_srv = RosHandlerDefinition(Talk, '/roboy/cognition/speech/synthesis/talk/german')
+speech_recognition_srv = RosHandlerDefinition(RecognizeSpeech, '/roboy/cognition/speech/recognition')
+
+emotion_pub = RosHandlerDefinition(Emotion, '/roboy/cognition/face/emotion')
