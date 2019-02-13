7aa95e3b6a21c56de3d2cfc3b070fa7667ac3fb2
missxa <aliona.kharchenko@gmail.com>
2019-02-13 16:45:31 +0100

    debug

--- pyroboy/face_recognition.py
+++ pyroboy/face_recognition.py
@@ -1,20 +1,41 @@
 import face_recognition as fr
+import math
+import numpy
 
 class FaceRec:
 
-    def get_face_encoding(img_path):
-        loaded_img = fr.load_image_file(img_path)
-        return fr.face_encodings(loaded_img)[0]
+    def face_distance_to_conf(face_distance, face_match_threshold=0.6):
+        if face_distance > face_match_threshold:
+            range = (1.0 - face_match_threshold)
+            linear_val = (1.0 - face_distance) / (range * 2.0)
+            return linear_val
+        else:
+            range = face_match_threshold
+            linear_val = 1.0 - (face_distance / (range * 2.0))
+            return linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
 
-    def match_face(new_face, known_faces, tolerance=0.5):
+    def get_biggest_face_encoding(img_path):
+        loaded_img = fr.load_image_file(img_path)
+        face_encodings = fr.face_encodings(loaded_img)
+        if len(face_encodings) == 0:
+            return None
+
+        face_locations = fr.face_locations(loaded_img)
+        face_sizes = []
+        for (top, right, bottom, left) in face_locations:
+            x = np.array((left, top))
+            y = np.array((right, bottom))
+            face_sizes.append(np.linalg.norm(x-y))
+        index_biggest = np.argmax(face_sizes)
+        return face_encodings[index_biggest]
+
+    def match_face(new_face, known_faces, face_match_threshold=0.6):
         '''
-        returns the first index of the known_faces that matches new_face
-        with given tolerance
+        returns the closest face index from known_faces and confidence in [0,1]
         '''
-        match = fr.compare_faces(known_faces, new_face, tolerance=tolerance)
-        idx = None
-        try:
-            idx = match.index(True)
-        except:
-            pass
-        return idx
+        distances = fr.face_distance(known_faces, new_face)
+        confindences = [face_distance_to_conf(d, face_match_threshold=face_match_threshold) for d in distances]
+        index_max = np.argmax(confidences)#max(xrange(len(confindences)), key=confindences.__getitem__)
+        print("max index: %i"%index_max)
+        print("conf: %d"confidences[index_max])
+        return index_max, confindences[index_max]
