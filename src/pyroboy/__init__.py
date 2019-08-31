302f3daba93ed4d5ff51ef0af14fb8dfb117f864
missxa <aliona.kharchenko@gmail.com>
2019-08-31 23:17:27 +0200

    added leds and talking flag

--- src/pyroboy/__init__.py
+++ src/pyroboy/__init__.py
@@ -2,13 +2,14 @@ import rospy
 import time
 from pyroboy.speech_synthesis import TTS
 from pyroboy.speech_recognition import STT
-from pyroboy.common_definitions import emotion_pub
+from pyroboy.common_definitions import emotion_pub, leds_pub
 
 class Pyroboy():
     def __init__(self):
         self.tts = None
         self.stt = None
         self.emotion_publisher = None
+        self.leds_publisher = None
 
 
     def init_tts(self):
@@ -20,20 +21,23 @@ class Pyroboy():
     def init_face(self):
         self.emotion_publisher = rospy.Publisher(emotion_pub.name, emotion_pub.type, queue_size=1)
 
+    def init_matrix(self):
+        self.leds_publisher = rospy.Publisher(leds_pub.name, leds_pub.type, queue_size=1)
+
 
 
 listen_start_timestamp = 0
 say_end_timestamp = 0
 
 def say(text, language="en"):
+    rospy.set_param('talking', True)
     if pr.tts is None:
         pr.init_tts()
-    global say_end_timestamp
     result = pr.tts.generate(text, language)
-    say_end_timestamp = time.time()
+    rospy.set_param('talking', False)
     return result
 
-def listen(discard_on_say=True):
+def listen(discard_on_say=False):
     if pr.stt is None:
         pr.init_stt()
     global say_end_timestamp
@@ -50,6 +54,23 @@ def show_emotion(emotion):
         pr.init_face()
     msg = emotion_pub.type(emotion=emotion)
     return pr.emotion_publisher.publish(msg)
+
+def leds(mode="off"):
+    if pr.leds_publisher is None:
+        pr.init_matrix()
+    if mode == "pulse":
+        mode_id = 1
+    elif mode == "rainbow":
+        mode_id = 4
+    elif mode == "tail":
+        mode_id = 2
+    elif mode == "police":
+        mode_id = 3
+    else:
+        mode_id = 0
+        print("unknow mode for matrix leds")
+    pr.leds_publisher.publish(mode_id)
+
 
 rospy.init_node('pyroboy')
 pr = Pyroboy()
--- src/pyroboy/common_definitions.py
+++ src/pyroboy/common_definitions.py
@@ -1,6 +1,7 @@
 import rospy
 from roboy_cognition_msgs.srv import Talk, TalkRequest, RecognizeSpeech, RecognizeSpeechRequest
 from roboy_control_msgs.msg import Emotion
+from std_msgs.msg import Int32
 
 class RosHandlerDefinition:
     def __init__(self, type, request, name):
@@ -14,3 +15,5 @@ speech_synthesis_de_srv = RosHandlerDefinition(Talk, TalkRequest, '/roboy/cognit
 speech_recognition_srv = RosHandlerDefinition(RecognizeSpeech, RecognizeSpeechRequest, '/roboy/cognition/speech/recognition')
 
 emotion_pub = RosHandlerDefinition(Emotion, None, '/roboy/cognition/face/emotion')
+
+leds_pub = RosHandlerDefinition(Int32, None, "/roboy/control/matrix/leds/mode/simple")
